6315655cf0683f3e9c7c1c5d234e76d68339aaf5
Abdul Basith <abdulbasith0792@gmail.com>
2025-07-23 18:54:25 -0700

    feat/single-email-manually-done

---
+++ scripts/email_sender.py
@@ -0,0 +1,47 @@
+import smtplib
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# env_path = Path(__file__).resolve().parents[1] / ".env"
+
+# from_email = os.getenv("EMAIL_USER")
+# app_password = os.getenv("EMAIL_PASS")
+
+# if not from_email or not app_password:
+#     raise Exception("❌ Email or App Password not found in environment variables!")
+
+
+
+# load_dotenv(dotenv_path=env_path)
+def email_send(subject, body, to_email, from_email, app_password, attachment_path=None):
+    message = EmailMessage()
+    message["Subject"] = subject
+    message["From"] = from_email
+    message["To"] = to_email
+    message.set_content(body)
+
+    if attachment_path and os.path.exists(attachment_path):
+        with open(attachment_path, "rb") as f:
+            file_data = f.read()
+            file_name = os.path.basename(attachment_path)
+            message.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(from_email, app_password)
+        smtp.send_message(message)
+
+    print("Email has been sent")
+
+
+if __name__ == "__main__":
+    subject = input("Subject: ")
+    body = input("Body: ")
+    to = input("Recipient Email: ")
+    attachment = input("attachment: ")
+    from_email = 'abdulbasith0792@gmail.com'
+    app_password = 'lhwb jmjx dqtn xobt'
+
+    email_send(subject, body, to, from_email, app_password, attachment or None)
+
